c7c755e30770a4774d21f833a5ac4a7fea3c2226
David Nieto <u2903149@unimilitar.edu.co>
2023-04-02 09:18:32 -0500

    Js-OrientadoObjetos

---
+++ index.js
@@ -0,0 +1,18 @@
+/* {
+    nombreCliente;
+    dniCliente;
+    numeroCuenta;
+    saldoCuenta;
+}
+ */
+const nombreCliente = "Leonardo";
+const dniCliente = "13804050";
+const numeroCuenta = "123434343";
+const saldoCuenta = 1000;
+
+const nombreCliente2 = "Leonardo";
+const dniCliente2 = "13804050";
+const numeroCuenta2 = "123434343";
+const saldoCuenta2 = 1000;
+
+console.log(nombreCliente);

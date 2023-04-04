ae79e55db95d45d3209d5ec3d1f48225b746273d
David Nieto <u2903149@unimilitar.edu.co>
2023-04-04 18:22:03 -0500

    Final-OPP

--- Cliente.js
+++ Cliente.js
@@ -3,4 +3,9 @@ export class Cliente
     nombreCliente;
     dniCliente;
     rutCliente;
+    constructor (nombreCliente,dniCliente,rutCliente){
+        this.nombreCliente = nombreCliente;
+        this.dniCliente = dniCliente;
+        this.rutCliente = rutCliente;
+    }
 }
